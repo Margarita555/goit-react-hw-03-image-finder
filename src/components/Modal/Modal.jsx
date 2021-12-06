5f8885782099a50d148bc82298efeb16399c2337
Margarita555 <a_rita@ukr.net>
2021-12-06 22:05:23 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,17 +1,12 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom/cjs/react-dom.development';
 import { ReactComponent as CloseButton } from '../../images/closeBtn.svg';
-import Spinner from '../Spinner/Spinner';
 import s from './Modal.module.css';
 const modalRoot = document.querySelector('#modal-root');
 
 export default class Modal extends Component {
-  state = {
-    image: '',
-  };
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
-    // this.setState({ image: '' });
   }
 
   componentWillUnmount() {
@@ -36,27 +31,12 @@ export default class Modal extends Component {
     }
   };
 
-  handleImageLoaded() {
-    this.setState({ image: 'loaded' });
-  }
-
-  // handleImageErrored() {
-  //   this.setState({ image: 'failed to load' });
-  // }
-
   render() {
     return createPortal(
       <div className={s.overlay} onClick={this.handleBackdropClick}>
         <CloseButton className={s.closeButton} fill="#fff" />
         <div className={s.modal}>
-          {!this.state.image && <Spinner />}
-          <img
-            src={this.props.largeImageURL}
-            alt="img"
-            onLoad={this.handleImageLoaded.bind(this)}
-            // onError={this.handleImageErrored.bind(this)}
-          />
-          {/* {this.state.image} */}
+          <img src={this.props.largeImageURL} alt="img" />
         </div>
       </div>,
       modalRoot,
